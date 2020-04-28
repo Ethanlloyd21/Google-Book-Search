891a397ff56314619b13adfda5b49c0ecd137f21
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-04-27 22:29:41 -0700

    update

--- client/src/componets/Footer/index.js
+++ client/src/componets/Footer/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="footer">
-            <span>React Google Boock Search 2020 | Lloyd Marcelino | portfolio: <a href="https://lloydmarcelino.com"> lloydmarcelino.com </a></span>
+            <span>React Google Book Search 2020 | Lloyd Marcelino | portfolio: <a href="https://lloydmarcelino.com"> lloydmarcelino.com </a></span>
         </footer>
     );
 }
